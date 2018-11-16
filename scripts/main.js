5a9328b494a5ac5007ba4b9883dddee119463df9
JanelleNaelle <44621074+JanelleNaelle@users.noreply.github.com>
2018-11-16 16:12:46 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,2 @@
 alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
-<script type="text/javascript" language="javascript">
-  if (confirm("Vous désirez vraiment quitter?")) {
-    alert("oui")
-  }
-  else {
-    alert("non")
-  }
-</script>
+
