c78425b2dd74d422e7726d1c2ecf4e9c2fe41c85
JanelleNaelle <44621074+JanelleNaelle@users.noreply.github.com>
2018-11-16 16:26:41 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,1 @@
-if (confirm("êtes vous d’accord")) { 
-alert("vous êtes d'accord")
-}
-else{
-alert("vous n'êtes pas d'accord")
-}
+confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
