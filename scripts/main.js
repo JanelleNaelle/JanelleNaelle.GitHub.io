a85527916a3d42bfea07048c61301c9b473eb24d
JanelleNaelle <44621074+JanelleNaelle@users.noreply.github.com>
2018-11-16 15:57:50 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+<form>
+<input type="button" value="Cliquez moi" onClick="Message()">
+</form>
+<script type="text/javascript">
+   function Message() {
+       var msg="Message sur la ligne 1.nMessage sur la ligne 2.n...";
+       console.log(msg)
+       alert(msg);
+   }
+</script>
 
