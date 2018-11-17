13b0c8dfe0a65eaf1cf4e634b4790632b8cdce7e
JanelleNaelle <44621074+JanelleNaelle@users.noreply.github.com>
2018-11-17 13:50:13 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 setTimeout(myFunction, 3000)
 
 confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+function finish(youtube.com){
+   window.opener.location.href=monUrl;
+   window.opener.focus();
+   window.close();
